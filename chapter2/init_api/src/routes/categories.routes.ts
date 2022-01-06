2904c099adfe749427d6d1f3262ff6ba839ddc49
Bruno <skyheads@live.com>
2022-01-06 00:14:53 -0300

    feat: findbyname repository categories

--- chapter2/init_api/src/routes/categories.routes.ts
+++ chapter2/init_api/src/routes/categories.routes.ts
@@ -8,6 +8,12 @@ const categoriesRepository = new CategoriesRepository()
 categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body
 
+  const findCategoryIfExists = categoriesRepository.findByName(name)
+
+  if (findCategoryIfExists) {
+    return res.status(400).json({ error: 'User already exists.' })
+  }
+
   const category = categoriesRepository.create({ name, description })
 
   return res.status(201).json(category)
