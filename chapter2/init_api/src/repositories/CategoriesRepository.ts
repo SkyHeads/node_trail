b7538f64e803eedc3c4bc076e1775834767b1eb7
Bruno <skyheads@live.com>
2022-01-06 00:09:40 -0300

    feat: list categories route

--- chapter2/init_api/src/repositories/CategoriesRepository.ts
+++ chapter2/init_api/src/repositories/CategoriesRepository.ts
@@ -25,6 +25,10 @@ class CategoriesRepository {
 
     return this.categories
   }
+
+  list(): Category[] {
+    return this.categories
+  }
 }
 
 export { CategoriesRepository }
--- chapter2/init_api/src/routes/categories.routes.ts
+++ chapter2/init_api/src/routes/categories.routes.ts
@@ -3,15 +3,20 @@ import { Router } from 'express'
 import { CategoriesRepository } from '../repositories/CategoriesRepository'
 
 const categoriesRoutes = Router()
+const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body
 
-  const categoriesRepository = new CategoriesRepository()
-
   const category = categoriesRepository.create({ name, description })
 
   return res.status(201).json(category)
+})
+
+categoriesRoutes.get('/', (req, res) => {
+  const categories = categoriesRepository.list()
+
+  return res.status(200).json(categories)
 })
 
 export { categoriesRoutes }
