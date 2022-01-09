a6f911ee20bbc4b3e3f048fa5229cd4deecd2fc4
Bruno <skyheads@live.com>
2022-01-09 02:51:27 -0300

    feat: add multer + route import

--- chapter2/init_api/src/routes/categories.routes.ts
+++ chapter2/init_api/src/routes/categories.routes.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express'
+import multer from 'multer'
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
 import { listCategoriesController } from '../modules/cars/useCases/listCategories'
 
 const categoriesRoutes = Router()
+const upload = multer({
+  dest: './tmp'
+})
+
+categoriesRoutes.post('/import', upload.single('file'), (req, res) => {
+  const { file } = req
+  console.log(file)
+  return res.status(200).json()
+})
 
 categoriesRoutes.post('/', (req, res) => {
   return createCategoryController.handle(req, res)
