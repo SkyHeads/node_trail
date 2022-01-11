2a6869c1880277e055ae6b86688b181b044adfa9
Bruno <skyheads@live.com>
2022-01-11 00:47:18 -0300

    feat: import category use case with stream + csv parse

--- chapter2/init_api/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ chapter2/init_api/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,7 +1,43 @@
+import { parse } from 'csv-parse'
+import fs from 'fs'
+
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository'
+
+interface IImportCategory {
+  name: string
+  description: string
+}
+
 class ImportCategoryUseCase {
+  // eslint-disable-next-line prettier/prettier
+  constructor(private categoriesRepository: ICategoriesRepository) { }
+
   // eslint-disable-next-line no-undef
-  execute(file: Express.Multer.File): void {
-    console.log(file)
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path)
+      const categories: IImportCategory[] = []
+      const parseFile = parse()
+
+      stream.pipe(parseFile)
+
+      parseFile
+        .on('data', async (line) => {
+          const [name, description] = line
+
+          categories.push({ name, description })
+        })
+        .on('end', () => {
+          resolve(categories)
+        })
+        .on('error', (err) => reject(err))
+    })
+  }
+
+  // eslint-disable-next-line no-undef
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file)
+    console.log(categories)
   }
 }
 
