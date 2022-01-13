f3dba19b425db9bbc7862e63f53060608efffc75
Bruno <skyheads@live.com>
2022-01-13 16:51:53 -0300

    refactor: change name models to entities

--- chapter3/continuing_api/src/modules/cars/repositories/ICategoriesRepository.ts
+++ chapter3/continuing_api/src/modules/cars/repositories/ICategoriesRepository.ts
@@ -1,4 +1,4 @@
-import { Category } from '../models/Category'
+import { Category } from '../entities/Category'
 import { ICreateCategories } from './implementations/CategoriesRepository'
 
 interface ICategoriesRepository {
