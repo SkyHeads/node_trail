3e5f0532d03bde82a3aac9b6efbbf6252f4d4b45
Bruno <skyheads@live.com>
2022-01-19 02:15:04 -0300

    feat: creat authenticate use case + sessions route

---
+++ chapter3/continuing_api/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,58 @@
+import { compare } from 'bcrypt'
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string
+  password: string
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  }
+  token: string
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) { }
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new Error('Email or Password Incorrect')
+    }
+
+    const checkPassword = await compare(password, user.password)
+
+    if (!checkPassword) {
+      throw new Error('Email or Password Incorrect')
+    }
+
+    const token = sign({}, "634dbcdcec995f2a01edce3dc03badb8", {
+      subject: user.id,
+      expiresIn: '1d'
+    })
+
+    delete user.created_at
+    delete user.updated_at
+    delete user.password
+    delete user.isAdmin
+    delete user.id
+
+    return {
+      user,
+      token
+    }
+  }
+}
+
+export { AuthenticateUserUseCase }
--- chapter3/continuing_api/src/routes/index.ts
+++ chapter3/continuing_api/src/routes/index.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express'
 
+import { authenticateRouter } from './authenticate.routes'
 import { categoriesRoutes } from './categories.routes'
 import { specificationsRouter } from './specifications.routes'
 import { usersRoutes } from './users.routes'
+
 
 const router = Router()
 
 router.use('/categories', categoriesRoutes)
 router.use('/specifications', specificationsRouter)
 router.use('/users', usersRoutes)
+router.use('/sessions', authenticateRouter)
 
 export { router }
