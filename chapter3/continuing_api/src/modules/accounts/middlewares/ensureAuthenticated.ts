c53bc36688fde3696be1134d83d1aa1856c8e5e4
Bruno <skyheads@live.com>
2022-01-20 16:51:57 -0300

    feat: create middleware ensureAuthenticated + verify jwt + add in routes

---
+++ chapter3/continuing_api/src/modules/accounts/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { UsersRepository } from "../repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string
+}
+
+export async function ensureAuthenticated(req: Request, res: Response, next: NextFunction): Promise<void> {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader) {
+    throw new Error('JWT is missing.')
+  }
+
+  const [, token] = authHeader.split(' ')
+
+  try {
+    const { sub: user_id } = verify(token, '634dbcdcec995f2a01edce3dc03badb8') as IPayload
+
+    const usersRepository = new UsersRepository()
+
+    const checkIfUserExists = await usersRepository.findById(user_id)
+
+    if (!checkIfUserExists) {
+      throw new Error('User does not exists!')
+    }
+
+    return next();
+  } catch (err) {
+    throw new Error(`JWT token is invalid, Message: ${err}`)
+  }
+}
--- chapter3/continuing_api/src/routes/index.ts
+++ chapter3/continuing_api/src/routes/index.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express'
 
+import { ensureAuthenticated } from '../modules/accounts/middlewares/ensureAuthenticated'
 import { authenticateRouter } from './authenticate.routes'
 import { categoriesRoutes } from './categories.routes'
 import { specificationsRouter } from './specifications.routes'
 import { usersRoutes } from './users.routes'
 
-
 const router = Router()
 
-router.use('/categories', categoriesRoutes)
-router.use('/specifications', specificationsRouter)
+router.use('/categories', ensureAuthenticated, categoriesRoutes)
+router.use('/specifications', ensureAuthenticated, specificationsRouter)
 router.use('/users', usersRoutes)
 router.use('/sessions', authenticateRouter)
 
