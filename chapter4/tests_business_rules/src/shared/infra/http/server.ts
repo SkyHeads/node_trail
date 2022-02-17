cc92f71082a4111fd881fd63cca8b7f54bcc1fed
Bruno <skyheads@live.com>
2022-02-17 01:29:46 -0300

    feature: create seed for user admin

--- chapter4/tests_business_rules/src/shared/infra/http/server.ts
+++ chapter4/tests_business_rules/src/shared/infra/http/server.ts
@@ -1,17 +1,18 @@
+import 'reflect-metadata'
 import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
 import { MulterError } from 'multer'
 import swaggerUi from 'swagger-ui-express'
 
-import '@shared/infra/typeorm'
-
 import '@shared/container'
 
 import { AppError } from '@shared/errors/AppError'
 import { router } from '@shared/infra/http/routes'
+import createConnection from '@shared/infra/typeorm'
 
 import swaggerFile from '../../../swagger.json'
 
+createConnection()
 const app = express()
 
 app.use(express.json())
@@ -20,8 +21,8 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(router)
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   (err: Error, req: Request, res: Response, _: NextFunction): Response => {
     if (err instanceof AppError) {
       return res.status(err.statusCode).json({
