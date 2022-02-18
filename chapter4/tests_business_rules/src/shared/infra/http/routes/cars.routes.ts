4e5132244e53415cf7470a7eac075c237e7ee3f8
Bruno <skyheads@live.com>
2022-02-18 01:50:25 -0300

    feat: create list available cars route

--- chapter4/tests_business_rules/src/shared/infra/http/routes/cars.routes.ts
+++ chapter4/tests_business_rules/src/shared/infra/http/routes/cars.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express'
 
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController'
+import { ListAvailableCarsController } from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController'
 
 import { ensureAdmin } from '../middlewares/ensureAdmin'
 
 const carRouter = Router()
 const createCarController = new CreateCarController()
+const listAvailableCarsController = new ListAvailableCarsController()
 
+carRouter.get('/available', listAvailableCarsController.handle)
 carRouter.post('/', ensureAdmin, createCarController.handle)
 
 export { carRouter }
